d16cef00073819b92f85ee5c7ed15ed3b91a484d
steeldragonn <sweetsourr.sos@gmail.com>
2024-09-09 16:49:45 +0200

    squareBox

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Footer from "./Footer";
-import Nav from "./components/Nav";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
